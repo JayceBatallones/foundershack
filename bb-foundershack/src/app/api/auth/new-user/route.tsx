aac1400c35903217889e721832ccf1a1189fb831
chuiwinnie <winniechui0106@gmail.com>
2024-08-02 21:38:19 +1000

    Implemented login functionality

---
+++ bb-foundershack/src/app/api/auth/new-user/route.tsx
@@ -0,0 +1,52 @@
+import { NextResponse } from 'next/server';
+import { currentUser, auth } from '@clerk/nextjs/server';
+import { prisma } from "@/lib/db";
+
+
+
+export async function GET() {
+  const { userId } = auth();
+  if (!userId) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  // Get user's information
+  const user = await currentUser();
+  if (!user) {
+    return new NextResponse('User not exist', { status: 404 });
+  }
+
+
+  let dbUser = await prisma.user.findUnique({
+    where: { clerkId: user.id },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        name: user.firstName ?? '',
+        lastName: user.lastName ?? '',
+        email: user.emailAddresses[0].emailAddress ?? '',
+      },
+    });
+  }
+
+  if (!dbUser) {
+    return new NextResponse(null, {
+      status: 302, // 302 Found - temporary redirect
+      headers: {
+        Location: 'http://localhost:3000/',
+      },
+    });
+  }
+  // Perform your Route Handler's logic with the returned user object
+
+  return new NextResponse(null, {
+    status: 302, // 302 Found - temporary redirect
+    headers: {
+      Location: 'http://localhost:3000/dashboard',
+    },
+  });
+
+}
