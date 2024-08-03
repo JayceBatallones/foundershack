abd42bb3220d4d8f837f36e5b37e50d9bab99595
stevehuynh <steve.huynh@monash.edu>
2024-08-03 16:24:48 +1000

    pathway button renderer almost complete

--- bb-foundershack/src/components/pathways/PathwayButtonRenderer.tsx
+++ bb-foundershack/src/components/pathways/PathwayButtonRenderer.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+"use client";
+import React from "react";
+import axios from "axios";
+import { checkCorrectAnswersSchema } from "@/schemas/pathway";
+import { z } from "zod";
 
 type Node = {
     name: string;
@@ -21,7 +25,106 @@ type Node = {
     userPathwayId: string;
     onLoadComplete: () => void; // New prop for callback
   };
-const PathwayButtonRenderer = (props: Props) => {
+
+  const PathwayButtonRenderer = ({
+    initialNodes = [],
+    currentTopicIndex,
+    currentQuestionIndex,
+    topicMap,
+    userPathwayId,
+    onLoadComplete,
+  }: Props) => {
+    const [nodes, setNodes] = React.useState<Node[]>(initialNodes);
+    const [loading, setLoading] = React.useState<boolean>(true);
+
+    React.useEffect(() => {
+        const fetchCorrectAnswers = async () => {
+          const updatedNodes = [...nodes];
+          try {
+            for (let index = 0; index < nodes.length; index++) {
+              const node = nodes[index];
+    
+              const payload: z.infer<typeof checkCorrectAnswersSchema> = {
+                type: node.type,
+                name: node.name,
+                threshold: node.threshold,
+                nodeAmount: nodes.length,
+                topicMap: topicMap,
+                userPathwayId: userPathwayId,
+                currentQuestionIndex: currentQuestionIndex,
+                currentTopicIndex:currentTopicIndex
+    
+              };
+    
+              let isLocked = true;
+              let current = false;
+    
+              if (
+                node.index === currentTopicIndex &&
+                index === currentQuestionIndex
+              ) {
+                const response = await axios.post(
+                  `/api/checkCorrectAnswers`,
+                  payload
+                );
+    
+                currentTopicIndex = response.data.currentTopicIndex;
+                currentQuestionIndex = response.data.currentNodeIndex;
+    
+                if (node.index < currentTopicIndex) {
+                  isLocked = false;
+                } else if (node.index === currentTopicIndex) {
+                  if (index > currentQuestionIndex) {
+                    isLocked = true;
+                  } else {
+                    isLocked = false;
+                    if (currentQuestionIndex === index) {
+                      current = true;
+                    }
+                  }
+                }
+    
+                updatedNodes[index] = {
+                  ...node,
+                  correctAnswers: response.data.correctAnswerCount,
+                  isLocked: isLocked,
+                  current: current,
+                };
+              } else {
+                if (node.index < currentTopicIndex) {
+                  isLocked = false;
+                } else if (node.index === currentTopicIndex) {
+                  if (index > currentQuestionIndex) {
+                    isLocked = true;
+                  } else {
+                    isLocked = false;
+                    if (currentQuestionIndex === index) {
+                      current = true;
+                    }
+                  }
+                }
+    
+                updatedNodes[index] = {
+                  ...node,
+                  correctAnswers: -1,
+                  isLocked: isLocked,
+                  current: current,
+                };
+              }
+            }
+    
+            setNodes(updatedNodes);
+          } catch (error) {
+            console.error("Error fetching correct answers:", error);
+          } finally {
+            setLoading(false);
+            onLoadComplete(); // Notify parent component that loading is complete
+          }
+        };
+    
+        fetchCorrectAnswers();
+      }, []);
+    
   return (
     <div>PathwayButtonRenderer</div>
   )
