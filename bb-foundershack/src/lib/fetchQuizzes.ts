387b3f8a6132bad0b898391b3badbc3f2b685b12
chuiwinnie <winniechui0106@gmail.com>
2024-08-03 15:38:04 +1000

    quiz bug fix

--- bb-foundershack/src/lib/fetchQuizzes.ts
+++ bb-foundershack/src/lib/fetchQuizzes.ts
@@ -1,10 +1,16 @@
 import { prisma } from './db';
 
-// TODO this needs to be changed to only fetch quizzes assigned to users
+// Fetch quizzes where duration is not equal to -1
 export const fetchQuizzes = async () => {
   try {
-    const quizList = await prisma.quiz.findMany();
-    
+    const quizList = await prisma.quiz.findMany({
+      where: {
+        duration: {
+          not: -1,
+        },
+      },
+    });
+
     return quizList;
   } catch (error) {
     console.error('Error fetching quizzes:', error);
