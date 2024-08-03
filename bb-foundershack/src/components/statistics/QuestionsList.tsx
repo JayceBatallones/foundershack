8561e2ce16db79ce4e1fec7a045c82f4fb5b6a54
chuiwinnie <winniechui0106@gmail.com>
2024-08-03 15:54:00 +1000

    Made quiz summary table collapsible

--- bb-foundershack/src/components/statistics/QuestionsList.tsx
+++ bb-foundershack/src/components/statistics/QuestionsList.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
 
+import React from "react";
 import {
   Table,
   TableBody,
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/table";
 import { Prisma } from "@prisma/client";
 import { MathJaxHtml } from "mathjax3-react";
+import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible";
 
 type Props = {
   attempt: {
@@ -42,37 +43,61 @@ const QuestionsList = ({ attempt, testQuestion }: Props) => {
           <TableHead className="w-[10px]">No.</TableHead>
           <TableHead>Question & Correct Answer</TableHead>
           <TableHead>Your Answer</TableHead>
+          <TableHead></TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {combinedData.map(
           ({ CorrectAnswer, Explanation, Description, userAnswer }, index) => {
             const isCorrect = CorrectAnswer === userAnswer;
+
             return (
-              <TableRow key={index}>
-                <TableCell className="font-medium">{index + 1}</TableCell>
-                <TableCell>
-                    {Description && (
-                        <MathJaxHtml html={Description} /> 
-                    )}
-                  
-                  <span className="italics mb-1">
-                  {Explanation && (
-                    <div className="italic mb-1">
-                      <MathJaxHtml html={Explanation} />
-                    </div>
-                  )}
-                  </span>
-                  <span className="font-semibold">Correct Answer: {CorrectAnswer}</span>
-                </TableCell>
-                <TableCell
-                  className={`${
-                    isCorrect ? "text-green-600" : "text-red-600"
-                  } font-semibold`}
-                >
-                  {userAnswer}
-                </TableCell>
-              </TableRow>
+              <Collapsible asChild>
+                <>
+                  <TableRow key={index}>
+                    <TableCell className="font-medium">{index + 1}</TableCell>
+                    <TableCell>
+                      {Description && <MathJaxHtml html={Description} />}
+                      <span className="italics mb-1">
+                        {Explanation && (
+                          <div className="italic mb-1">
+                            <MathJaxHtml html={Explanation} />
+                          </div>
+                        )}
+                      </span>
+                      <span className="font-semibold">
+                        Correct Answer: {CorrectAnswer}
+                      </span>
+                    </TableCell>
+                    <TableCell
+                      className={`${
+                        isCorrect ? "text-green-600" : "text-red-600"
+                      } font-semibold`}
+                    >
+                      {userAnswer}
+                    </TableCell>
+                    <TableCell>
+                      <span>Expand</span>
+                    </TableCell>
+                  </TableRow>
+                  <CollapsibleContent asChild>
+                    <TableRow>
+                      <TableCell colSpan={4}>
+                        <span className="font-semibold">
+                          Further Explanation:
+                        </span>
+                        <div className="bg-gray-100 my-2 p-3 rounded">
+                          {/* TODO: Change to AI-generated explanation */}
+                          This section will show the AI-generated explanation.
+                          This section will show the AI-generated explanation.
+                          This section will show the AI-generated explanation.
+                          This section will show the AI-generated explanation.
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  </CollapsibleContent>
+                </>
+              </Collapsible>
             );
           }
         )}
